739e330624f0c3bde41bbaf5130235c7c81e3e42
Bryton <63312484+bryton90@users.noreply.github.com>
2022-04-11 19:16:35 +0300

    Add files via upload

---
+++ main(1).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int x, y, sum, product, difference;
+
+float quotient;
+cout <<"Enter the value of X: ";
+cin >> x;
+
+cout <<"Enter the value of Y: ";
+cin >> y;
+
+sum = x+y;
+cout << sum << endl;
+
+product = x*y;
+cout << product << endl;
+
+difference = x-y;
+cout << difference << endl;
+
+quotient = x/y;
+cout << quotient << endl;
+
+return 0;
+
+}
